3858422c9de16c310bc046f76da1fb20e5a12e1e
CobarrubiasJoshua1999 <qjbcobarrubias@tip.edu.ph>
2021-11-01 06:34:01 +0000

    with comments

--- mapquest.py
+++ mapquest.py
@@ -1,9 +1,12 @@
+#Importing Libraries
 import urllib.parse
 import requests
 
+#Declaring API and key variables 
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "oUnfyQT0lWqlhv6zAohIjpXPibSrWQH3"
 
+#Converting length
 def metric(dist):
     if unit_metric== "mi" or unit_metric=="miles" or unit_metric=="Miles":
         distance = dist
@@ -14,6 +17,7 @@ def metric(dist):
 
     return distance
 
+#Converting time
 def convert(timeRoute):
     if unit_time== "s" or unit_time=="seconds" or unit_time=="Seconds":
         time = timeRoute
@@ -23,7 +27,7 @@ def convert(timeRoute):
         time = timeRoute / 3600
 
     return time
-
+#Asking user input of starting location & destination
 while True:
     orig = input("Starting Location: ")
     if orig == "quit" or orig == "q":
@@ -31,7 +35,7 @@ while True:
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
         break
-    
+ #Asking user input of (m,km,mi)   
     unit_metric = input("Choose unit of length [ m | km | mi ]: ") 
     if unit_metric == "quit" or unit_metric == "q": 
         break
@@ -44,7 +48,7 @@ while True:
     else:
         print("Invalid input!")
         break
-
+#Asking user input of time prefernce 
     unit_time = input("Choose unit of time [ s | min | hr ]: ") 
     if unit_time == "quit" or unit_time == "q": 
         break
@@ -57,23 +61,23 @@ while True:
     else:
         print("Invalid input!")
         break
-
+ #Asking user input of route type       
     routeType = input("Choose route type [ fastest | shortest | pedestrian | bicycle ]: ")
     if routeType in ('fastest','shortest','pedestrian','bicycle'): pass
-    
+#Asking the user what road types to avoid   
     avoid = input ("What do you want to avoid? [ Limited Access Highway | Toll Road | Ferry | Unpaved | Approximate Seasonal Closure | Country Border Crossing | Bridge | Tunnel | None ]: ")
     if avoid in ('Limited Access Highway' , 'Toll Road', 'Ferry' , 'Unpaved' , 'Approximate Seasonal Closure' , 'Country Border Crossing' , 'Bridge' , 'Tunnel', 'None'): pass
     else:
         print("Invalid input!")
         break
-
+#Calling the API
     if avoid == 'None': url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest, "routeType":routeType})
     else: url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest, "routeType":routeType,"avoids":avoid}) 
     json_data = requests.get(url).json()
     print("URL: " + (url))
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
-    
+    #Printing the output parameters
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=================================================")
@@ -89,17 +93,17 @@ while True:
             distance = metric(each["distance"])
             print((each["narrative"]) + " (" + str("{:.2f}".format(distance) + " " + unit + ")"))
         print("=================================================")
-
+    #Error handling if user inputted wrong values on location and destination
     elif json_status == 402:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("**********************************************\n")
-
+    #Error handling if user has no input of destinaion and location
     elif json_status == 611:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         print("**********************************************\n")
-
+    #Error handling for other API status code 
     else:
         print("************************************************************************")
         print("For Staus Code: " + str(json_status) + "; Refer to:")
