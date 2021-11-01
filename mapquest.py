839715fc985c13456cdba10358baa25ca86f89a6
abcdalistan <abcdalistan@gmail.com>
2021-11-01 04:01:42 +0000

    edited time duration

--- mapquest.py
+++ mapquest.py
@@ -68,7 +68,7 @@ while True:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=================================================")
         print("Directions from " + (orig) + " to " + (dest))
-        print("Trip Duration: " + str("{}".format(time) + " " + time_unit))
+        print("Trip Duration: " + str("{}".format(time) + " " + time_unit + " | " +(json_data["route"]["formattedTime"])))
         print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=================================================")
