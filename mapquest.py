15ec383cebc6a390b1409009fdc5068cbeb794fb
abcdalistan <abcdalistan@gmail.com>
2021-11-01 04:15:02 +0000

    modified time duration

--- mapquest.py
+++ mapquest.py
@@ -8,9 +8,9 @@ def metric(dist):
     if unit_metric== "mi" or unit_metric=="miles" or unit_metric=="Miles":
         distance = dist
     elif unit_metric=="km" or unit_metric=="kilometer" or unit_metric=="Kilometer":
-        distance =dist * 1.61
+        distance =dist * 1.609
     elif unit_metric== "m" or unit_metric=="meter" or unit_metric=="Meter":
-        distance = dist * 1610
+        distance = dist * 1609
 
     return distance
 
@@ -18,9 +18,9 @@ def convert(timeRoute):
     if unit_time== "s" or unit_time=="seconds" or unit_time=="Seconds":
         time = timeRoute
     elif unit_time== "min" or unit_time=="minutes" or unit_time=="Minutes":
-        time = timeRoute * 0.016
+        time = timeRoute / 60
     elif unit_time== "hr" or unit_time=="hours" or unit_time=="Hours":
-        time = timeRoute * 0.0002
+        time = timeRoute / 3600
 
     return time
 
@@ -63,12 +63,13 @@ while True:
     print("URL: " + (url))
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
-    time = convert(json_data["route"]["time"])
+    
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=================================================")
         print("Directions from " + (orig) + " to " + (dest))
-        print("Trip Duration: " + str("{}".format(time) + " " + time_unit + " | " +(json_data["route"]["formattedTime"])))
+        time = convert(json_data["route"]["time"])
+        print("Trip Duration: " + str("{:.2f}".format(time) + " " + time_unit + " | " +(json_data["route"]["formattedTime"])))
         print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=================================================")
@@ -76,7 +77,6 @@ while True:
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             distance = metric(each["distance"])
             print((each["narrative"]) + " (" + str("{:.2f}".format(distance) + " " + unit + ")"))
-           # print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
         print("=============================================")
 
     elif json_status == 402:
